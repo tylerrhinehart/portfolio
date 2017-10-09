41265f3114aa63cfc32688b17a57d6ea869c9d27
Tyler Rhinehart <rhinehart22@gmail.com>
2017-10-09 09:31:28 -0600

    merged

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <v-list class="pa-0">
           <v-list-tile avatar>
             <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+              <img src="../static/images/square-headshot.jpg" />
             </v-list-tile-avatar>
             <v-list-tile-content>
               <v-list-tile-title>Tyler Rhinehart</v-list-tile-title>
